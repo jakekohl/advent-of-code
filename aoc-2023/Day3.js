2306712cfe7d0962b93283988921a588aecb3bce
Jake Kohl <jake@tagboard.com>
2023-12-05 21:35:02 -0600

    progress on day3 pt2 - need to figure out how to get gear part numbers

--- aoc-2023/Day3.js
+++ aoc-2023/Day3.js
@@ -18,8 +18,21 @@ Here is an example engine schematic:
 In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic? */
-
-const schematicInput = `..224.....487...................718.....................378............................................284........310......313..........311.
+let schematicInput = '';
+if (process.argv[2] == 'sample') {
+    schematicInput = `467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..`;
+}
+else {
+    schematicInput = `..224.....487...................718.....................378............................................284........310......313..........311.
 ....*..............................*744....486*485......*......741......@...359.#666...439................*925....*......$..+........@......
 .235................758..440...........................251....*......262.....*..........*......................752......774.......515.......
 .........705%..@746........+..942*591.347.470...#..257.........637...........793.......299..../.....813....509......464......&.........688..
@@ -159,6 +172,10 @@ const schematicInput = `..224.....487...................718.....................
 .........569*....*........975*.....*....968..............585.....*................26.................................394....@.142...........
 ......*......498..969.........360.666...%.........................919.......360........-.*.........%...................................484..
 ...407................886...................................84......................933...101....58........839..425.........................`;
+};
+
+
+
 
 // init
 const schArray = schematicInput.split("\n");
@@ -417,3 +434,83 @@ console.log(sumArray);
 const sumOfPartNumbers = sumArray.reduce((a, b) => a + b, 0);
 
 console.log(`Sum of Part Numbers is ${sumOfPartNumbers}`);
+
+
+
+/*
+--- Part Two ---
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+
+What is the sum of all of the gear ratios in your engine schematic?
+*/
+
+
+let arrayOfRatios = [];
+for (let r = 0; r < schArray.length; r++) {
+    // Find * characters and determine if they are adjacent to two part numbers
+    let rowVals = schArray[r];
+    let asterikArray = [];
+    let valueIndex = 0;
+    let preValIndex = 0;
+    asterikArray = rowVals.match(/\*/g);
+    
+    if (asterikArray !== null) {
+        for(let ac = 0; ac < asterikArray.length; ac++) {
+            let intCount = 0;
+            let gearPartNumbers = [];
+            let star = schDict[r * schArray.length + rowVals.indexOf('*',valueIndex)];
+            valueIndex = +(star["index"])+1;
+            let starNeighbors = star["neighbors"];
+            let intEval = Object.keys(starNeighbors);
+            intEval.forEach(function(starDirect) {
+                let neighEval = starNeighbors[starDirect];
+                if (starDirect !== undefined) {
+                    if (neighEval['typeOf'] == 'int') {
+                        intCount++;
+                        let findPartNumber = schArray[neighEval["row"]];
+                        let getPartNumbers = findPartNumber.match(/\d+/g); 
+                        let neighEvalVal = neighEval["value"];
+                        let regex = `/${neighEvalVal}/g`
+                        let numbersInclude = getPartNumbers.filter((members) => members.match(regex));
+                        gearPartNumbers.push(neighEval["value"]);
+                    };
+                };
+                if (intCount > 2) {
+                    ac = asterikArray.length;
+                };
+            });
+            if (intCount == 2 && gearPartNumbers.length == 2) {
+                let gearRatioVal = gearPartNumbers[0] * gearPartNumbers[1];
+                arrayOfRatios.push(gearRatioVal);
+            }
+        };
+    };
+};
+
+console.log(arrayOfRatios);
+
+const sumOfRatios = arrayOfRatios.reduce((a, b) => a + b, 0);
+
+console.log(`Sum of all gear ratios is ${sumOfRatios}`);
